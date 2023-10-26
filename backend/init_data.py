d71c4168016974b0bde1f1fd0f668207a830161e
axjing <34448205+axjing@users.noreply.github.com>
2023-10-26 19:01:39 +0800

    解决SQL链接异常

--- backend/init_data.py
+++ backend/init_data.py
@@ -10,9 +10,11 @@
 
 
 from db.crud import create_user
-from db.schemas import UserCreate
-from db.session import SessionLocal, get_db
+from app.db import models
+from app.db.schemas import UserCreate
+from app.db.session import SessionLocal, engine, get_db
 
+models.Base.metadata.create_all(bind=engine)
 
 def init() -> None:
     db = SessionLocal()
--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,52 @@ from core import config
 from fastapi import Depends, FastAPI
 from starlette.requests import Request
 
+from .db.session import SessionLocal, engine
+from .db import crud, models, schemas
 app = FastAPI(
     title=config.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api"
 )
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+#依赖
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # 根据email查找用户
+    db_user = crud.get_user_by_email(db, email=user.email)
+    # 如果用户存在，提示该邮箱已经被注册
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    # 返回创建的user对象
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/read/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    # 读取指定数量用户
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    # 获取当前id的用户信息
+    db_user = crud.get_user(db, user_id=user_id)
+    # 如果没有信息，提示用户不存在
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
